ed300611535ad642f20deab72de764dcd026b0ab
jerozapata <159493286+jerozapata@users.noreply.github.com>
2024-02-20 10:29:58 -0500

    toma 4

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Asiento:
             self.color = color
 
 class Auto:
-    cantidadCreados = 0
+    CANTIDAD_CREADOS = 0
 
     def __init__(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
